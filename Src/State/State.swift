9faeb39a276b3923bbea5a6e7a6d3be267c3038e
Maxim Khatskevich <maxim@khatskevi.ch>
2017-06-13 03:46:07 +0500

    Added inline header documentation for all members

--- Src/State/State.swift
+++ Src/State/State.swift
@@ -1,7 +1,9 @@
 import Foundation
 
 //===
-
+/**
+ Conformance to this protocl turns the type into a state.
+ */
 public
 protocol State { }
 
@@ -10,6 +12,25 @@ protocol State { }
 public
 extension State
 {
+    /**
+     Unique identifier of the state.
+     
+     Returns: An instance of `String` that contains full name of the type/state, inclduing module name and enclosing types (if this is a nested type).
+     
+     Use as follows (assume the module name is "MyApp"):
+     
+     ```
+     class MyView: UIView, Stateful
+     {
+         struct Normal: State { }
+     }
+     //...
+     let aView = MyView()
+     aView.state = MyView.Normal()
+     print(type(of: aView.state).id) // MyApp.MyView.Normal
+     ```
+     
+     */
     static
     var id: String { return String(reflecting: self) }
 }
