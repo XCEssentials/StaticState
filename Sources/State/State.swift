6659e72d873b392cc484462863850bd3d2661a36
Maxim Khatskevich <maxim@khatskevi.ch>
2017-06-29 01:09:06 +0500

    Changes in how we compare two states

--- Sources/State/State.swift
+++ Sources/State/State.swift
@@ -4,9 +4,45 @@ import Foundation
  Conformance to this protocl turns the type into a state.
  */
 public
-protocol State
+protocol State: Equatable
 {
+    /**
+     Reference-based type to which this state might be applied.
+     */
     associatedtype Owner: AnyObject
+    
+    /**
+     Unique identifier of the state.
+     
+     Having this on instance level gives opportunity to distinct different instances of the same type, if needed.
+     
+     Use as follows (assume the module name is "MyApp"):
+     
+     ```swift
+     class MyView: UIView, Stateful
+     {
+         struct Normal: State { typealias Owner = MyView }
+     }
+     
+     let aView = MyView()
+     aView.state = MyView.Normal()
+     print(type(of: aView.state).identifier) // MyApp.MyView.Normal
+     ```
+     */
+    var identifier: String { get }
+}
+
+/**
+ Default implementation of `Equatable` protocol.
+ */
+extension State
+{
+    public
+    static
+    func == (left: Self, right: Self) -> Bool
+    {
+        return left.identifier == right.identifier
+    }
 }
 
 //===
@@ -15,23 +51,11 @@ public
 extension State
 {
     /**
-     Unique identifier of the state.
-     
-     Returns: An instance of `String` that contains full name of the type/state, inclduing module name and enclosing types (if this is a nested type).
-     
-     Use as follows (assume the module name is "MyApp"):
-     
-     ```swift
-     class MyView: UIView, Stateful
-     {
-         struct Normal: State { }
-     }
-     
-     let aView = MyView()
-     aView.state = MyView.Normal()
-     print(type(of: aView.state).identifier) // MyApp.MyView.Normal
-     ```
+     Default implementation of `identifier` instance level property is based on recommended behavior when any two instances of the same state have identical identifiers, regardless of any internal data/properties.
      */
-    static
-    var identifier: String { return String(reflecting: self) }
+    var identifier: String
+    {
+        return "Owner: \(String(reflecting: Owner.self)),"
+            + " state: \(String(reflecting: type(of: self)))"
+    }
 }
